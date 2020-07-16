e265b9025cb3a75e9c54c985a5cdfd01fb9a5c44
Blake Smith <blakesmith0@gmail.com>
2020-07-16 10:53:55 -0500

    Define contract between UI, and executing the patch command

--- src/rombp.c
+++ src/rombp.c
@@ -4,7 +4,7 @@
 #include "ips.h"
 #include "ui.h"
 
-void close_files(FILE* input_file, FILE* output_file, FILE* ips_file) {
+static void close_files(FILE* input_file, FILE* output_file, FILE* ips_file) {
     if (input_file != NULL) {
         fclose(input_file);
     }
@@ -16,23 +16,23 @@ void close_files(FILE* input_file, FILE* output_file, FILE* ips_file) {
     }
 }
 
-int main() {
+static int patch_file(rombp_patch_command* command) {
     fprintf(stdout, "Patching file\n");
 
-    FILE* input_file = fopen("fixtures/Super Metroid (JU) [!].smc", "r");
+    FILE* input_file = fopen(command->input_file, "r");
     if (input_file == NULL) {
         fprintf(stderr, "Failed to open input file: %d\n", errno);
         return errno;
     }
 
-    FILE* output_file = fopen("Ascent.Super Metroid (JU) [!].smc", "w");
+    FILE* output_file = fopen(command->output_file, "w");
     if (output_file == NULL) {
         fprintf(stderr, "Failed to open output file: %d\n", errno);
         close_files(input_file, NULL, NULL);
         return errno;
     }
 
-    FILE* ips_file = fopen("fixtures/Ascent1.12.IPS", "r");
+    FILE* ips_file = fopen(command->ips_file, "r");
     if (ips_file == NULL) {
         fprintf(stderr, "Failed to open IPS file: %d\n", errno);
         close_files(input_file, output_file, NULL);
@@ -50,4 +50,14 @@ int main() {
 
     fprintf(stdout, "Done patching file, hunk count: %d\n", hunk_count);
     return 0;
+}
+
+int main() {
+    rombp_patch_command command;
+
+    command.input_file = "fixtures/Super Metroid (JU) [!].smc";
+    command.output_file = "Ascent.Super Metroid (JU) [!].smc";
+    command.ips_file = "fixtures/Ascent1.12.IPS";
+
+    return patch_file(&command);
 }
--- src/ui.c
+++ src/ui.c
@@ -1,9 +1,9 @@
 #include "ui.h"
 
-int ui_start(rombp_sdl* sdl) {
-    sdl->screen_width = 320;
-    sdl->screen_height = 240;
-    sdl->scaling_factor = 1.0;
+int ui_start(rombp_ui* ui) {
+    ui->sdl->screen_width = 320;
+    ui->sdl->screen_height = 240;
+    ui->sdl->scaling_factor = 1.0;
     
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -13,27 +13,73 @@ int ui_start(rombp_sdl* sdl) {
         fprintf(stderr, "SDL could not initialize TTF! TTF_Error: %s\n", TTF_GetError());
         return -1;
     }
-    sdl->window = SDL_CreateWindow("rombp",
+    ui->sdl->window = SDL_CreateWindow("rombp",
                                    SDL_WINDOWPOS_UNDEFINED,
                                    SDL_WINDOWPOS_UNDEFINED,
-                                   sdl->screen_width * sdl->scaling_factor,
-                                   sdl->screen_height * sdl->scaling_factor,
+                                   ui->sdl->screen_width * ui->sdl->scaling_factor,
+                                   ui->sdl->screen_height * ui->sdl->scaling_factor,
                                    SDL_WINDOW_SHOWN);
-    if (sdl->window == NULL) {
+    if (ui->sdl->window == NULL) {
         fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
 
-    sdl->renderer = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
-    if (sdl->renderer == NULL) {
+    ui->sdl->renderer = SDL_CreateRenderer(ui->sdl->window, -1, SDL_RENDERER_ACCELERATED);
+    if (ui->sdl->renderer == NULL) {
         fprintf(stderr, "Could not initialize renderer: SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
 
-    if (SDL_RenderSetScale(sdl->renderer, sdl->scaling_factor, sdl->scaling_factor) < 0) {
+    if (SDL_RenderSetScale(ui->sdl->renderer, ui->sdl->scaling_factor, ui->sdl->scaling_factor) < 0) {
         fprintf(stderr, "Could not set the renderer scaling: SDL_Error: %s\n", SDL_GetError());
         return -1;
     }
 
     return 0;
+}
+
+static void ui_resize_window(rombp_ui* ui, int width, int height) {
+    ui->sdl->screen_width = width;
+    ui->sdl->screen_height = height;
+}
+
+rombp_ui_event ui_handle_event(rombp_ui* ui, rombp_patch_command* command) {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event) != 0) {
+        switch (event.type) {
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                    case SDLK_q:
+                        return EV_QUIT;
+                    default:
+                        break;
+                }
+                break;
+            case SDL_WINDOWEVENT: {
+                switch (event.window.event) {
+                    case SDL_WINDOWEVENT_SIZE_CHANGED: {
+                        int w = event.window.data1;
+                        int h = event.window.data2;
+                        ui_resize_window(ui, w, h);
+                        printf("Window size is: %dx%d\n", w, h);
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                break;
+            }
+            case SDL_QUIT:
+                return EV_QUIT;
+            default:
+                break;
+        }
+    }
+
+    return EV_NONE;
+}
+
+void ui_draw(rombp_ui* ui) {
 }
