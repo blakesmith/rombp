25b8bff57e7717fbc56757157d6f856b0a8a7e32
Blake Smith <blakesmith0@gmail.com>
2020-08-01 09:07:59 -0500

    Make errors negative to prevent accidental status code check bugs

--- src/patch.h
+++ src/patch.h
@@ -5,8 +5,8 @@ typedef enum rombp_patch_type {
 
 typedef enum rombp_patch_err {
     PATCH_OK = 0,
-    PATCH_INVALID_HEADER = 1,
-    PATCH_ERR_IO = 2,
+    PATCH_INVALID_HEADER = -1,
+    PATCH_ERR_IO = -2,
 } rombp_patch_err;
 
 typedef enum rombp_hunk_iter_status {
